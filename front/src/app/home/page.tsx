9552ea272f2369b198569c61b167023f9410a0e3
EmilianoAllende <emilianoallende94@gmail.com>
2025-06-06 17:55:30 -0300

    Navbar and sidebar in develop.

---
+++ front/src/app/home/page.tsx
@@ -0,0 +1,8 @@
+export default async function Home() {
+    return (
+        <div className="bg-base1 rounded-xl text-white p-14 border-base3 border-opacity-30 border-l-2 border-t-2">
+            <div className="mx-auto flex flex-wrap gap-6 lg:gap-8 justify-center">
+            </div>
+        </div>
+    );
+};
--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -1,62 +1,11 @@
 "use client";
 
-import Link from "next/link";
-import Image from "next/image";
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "@/helpers/authContext";
-import Cart from "../Cart";
-import { getProducts } from "@/services/getProducts";
-import { IProduct } from "@/interfaces/Product";
-import SearchBar from "../SearchBar";
-import { UserWidget } from "./UserWidget/UserWidget";
-import userIcon from '@/utils/user-circle.svg'
 
 export default function Navbar() {
-    const { user } = useContext(AuthContext);
-    const [products, setProducts] = useState<IProduct[]>([]);
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-        try {
-            const allProducts = await getProducts();
-            setProducts(allProducts);
-        } catch (error) {
-            console.error("Error loading products", error);
-        }
-        };
-        fetchProducts();
-    }, []);
 
     return (
-        <nav className="flex w-10/12 right-0 bg-second fixed text-base1 items-center z-10 h-16 justify-around px-2">
-                    <Link href="/about" className="m-auto">
-                        <h3 className="font-black bg-base1 text-second rounded-full px-2">ABOUT</h3>
-                    </Link>
+        <nav className="bg-primary flex w-10/12 right-0 bg-second fixed text-base1 items-center z-10 h-16 justify-around px-2">
 
-                    <div className="w-auto m-auto">
-                        <SearchBar products={products} />
-                    </div>
-
-                    <Cart />
-
-                    <div className="flex m-auto">
-                        {user && (
-                            <Link href="/favorites" className="text-base1 hover:underline m-auto">
-                                <h3 className="font-black bg-base1 text-second rounded-full px-2">FAVORITES</h3>
-                            </Link>
-                        )}
-                    </div>
-
-
-                    <div className="flex my-auto ml-auto">
-                        {user && (
-                            <Link href="/dashboard"  className="mr-2">
-                                <Image src={userIcon} alt="userIcon" className="w-8" />
-                                <p>{user.name}</p>
-                            </Link>
-                        )}
-                        <UserWidget />
-                    </div>
         </nav>
     );
 };
